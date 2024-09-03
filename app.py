6a82094af60e2b9648449b3962a81cd17ee6bee6
Gaelle Reminiac <gaelle.rem@gmail.com>
2024-09-03 16:02:30 +0200

    hashed password with werkzeug

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import FlaskForm
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Email, InputRequired
 
 db = SQLAlchemy()
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.instance_path, 'database.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+    os.path.join(app.instance_path, 'database.db')
 app.config["SECRET_KEY"] = 'cgLN0zPgBqcN1xNjnKfma7oM2ZLkPd5D'
 db.init_app(app)
 migrate = Migrate(app, db)
@@ -20,13 +22,25 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     role = db.Column(db.String(80), nullable=False)
     created = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship('Post', backref='author', lazy=True, cascade="all, delete-orphan")
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<User "{self.username}">'
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,6 +52,7 @@ class Post(db.Model):
     def __repr__(self):
         return f'<Post "{self.title}">'
 
+
 class EditUserForm(FlaskForm):
     username = StringField("Votre pseudo", validators=[InputRequired()])
     email = StringField("Votre email", validators=[Email()])
@@ -45,13 +60,15 @@ class EditUserForm(FlaskForm):
 
 
 class SignUpForm(EditUserForm):
-    password = PasswordField('Votre mot de passe', validators=[InputRequired()])
+    password = PasswordField('Votre mot de passe',
+                             validators=[InputRequired()])
     submit = SubmitField("Créer votre compte")
 
 
 class SignInForm(FlaskForm):
     username = StringField("Votre pseudo", validators=[InputRequired()])
-    password = PasswordField('Votre mot de passe', validators=[InputRequired()])
+    password = PasswordField('Votre mot de passe',
+                             validators=[InputRequired()])
     submit = SubmitField("Créer votre compte")
 
 
@@ -60,10 +77,12 @@ def index():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
+
 @app.route('/users')
 def users():
     users = User.query.all()
     return render_template('users.html', users=users)
+
 
 @app.route('/user/<int:user_id>/edit', methods=('GET', 'POST'))
 def edit_user(user_id):
@@ -81,6 +100,7 @@ def edit_user(user_id):
     form.email.data = user.email
     return render_template('edit_user.html', user=user, form=form)
 
+
 @app.route('/user/<int:user_id>/delete', methods=('POST',))
 def delete_user(user_id):
     user = User.query.get_or_404(user_id)
@@ -89,15 +109,16 @@ def delete_user(user_id):
     flash('User was successfully deleted!', 'success')
     return redirect(url_for('users'))
 
+
 @app.route('/signup', methods=('GET', 'POST'))
 def signup():
     form = SignUpForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user is None:
-            password = bcrypt.hashpw(form.password.data.encode('utf-8'), bcrypt.gensalt())
+            password = form.password.data
             user = User(
-                username=form.username.data, 
+                username=form.username.data,
                 email=form.email.data,
                 password=password,
                 role='user'
@@ -125,11 +146,13 @@ def create_post():
         if not title:
             flash('Title is required!')
         else:
-            post = Post(title=title, content=content, user_id=1)  # Remplacez 1 par l'ID de l'utilisateur actuel
+            # Remplacez 1 par l'ID de l'utilisateur actuel
+            post = Post(title=title, content=content, user_id=1)
             db.session.add(post)
             db.session.commit()
             return redirect(url_for('index'))
     return render_template('create_post.html')
+
 
 @app.route('/post/<int:post_id>/edit', methods=('GET', 'POST'))
 def edit_post(post_id):
@@ -149,6 +172,7 @@ def edit_post(post_id):
 
     return render_template('edit_post.html', post=post)
 
+
 @app.route('/post/<int:post_id>/delete', methods=('POST',))
 def delete_post(post_id):
     post = Post.query.get_or_404(post_id)
@@ -157,13 +181,16 @@ def delete_post(post_id):
     flash('Post was successfully deleted!', 'success')
     return redirect(url_for('index'))
 
+
 @app.route('/admin')
 def admin():
     return render_template('admin.html')
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def page_not_found(e):
