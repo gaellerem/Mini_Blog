385ba46d87b7bf7b52d88c6b7110f555be91e008
Gaelle Reminiac <gaelle.rem@gmail.com>
2024-09-11 15:36:51 +0200

    access to details + overriding user's details page

--- app/admin_views.py
+++ app/admin_views.py
@@ -4,7 +4,6 @@ from flask_admin.contrib.sqla import ModelView
 from flask_login import current_user
 from wtforms import PasswordField
 from wtforms.validators import EqualTo
-from wtforms_alchemy.fields import QuerySelectField
 from app.models import User, Post
 from app.webforms import CreateUserForm
 
@@ -15,12 +14,13 @@ class IndexView(AdminIndexView):
         if not (current_user.is_authenticated and current_user.is_admin):
             return redirect(url_for('users.login', next=request.url))
 
+        admin_count = User.query.filter_by(is_admin=True).count()
         user_count = User.query.count()
         post_count = Post.query.count()
         recent_users = User.query.order_by(User.created.desc()).limit(5).all()
         recent_posts = Post.query.order_by(Post.created.desc()).limit(5).all()
 
-        return self.render('admin/index.html', user_count=user_count, post_count=post_count, recent_users=recent_users, recent_posts=recent_posts)
+        return self.render('admin/index.html', admin_count=admin_count, user_count=user_count, post_count=post_count, recent_users=recent_users, recent_posts=recent_posts)
 
     def is_accessible(self):
         return current_user.is_authenticated and current_user.is_admin
@@ -38,25 +38,30 @@ class AdminModelView(ModelView):
 
 
 class UserAdmin(AdminModelView):
-    def _format_created(view, context, model, name):
-        return model.created.strftime("%d-%m-%Y %H:%M")
+    column_formatters = { 
+        'created': lambda v, c, m, p: m.created.strftime("%d-%m-%Y"),
+        'posts': lambda v, c, m, p: len(m.posts)
+    }
+    column_labels = {
+        'id': 'ID', 
+        'username': 'Pseudo', 
+        'email': 'Adresse mail',
+        'created': "Membre depuis",
+        'posts': "Articles",
+        "is_admin": "Est admin",
+    }
+    column_list = ('id', 'username', 'email', 'created', 'posts', 'is_admin')
+    column_sortable_list = ('id', 'username', 'email', 'created', 'posts', 'is_admin')
 
-    column_list = ('id', 'username', 'email', 'created', 'is_admin')
-    column_labels = {'id': 'ID', 'username': 'Pseudo', 'email': 'Adresse mail',
-                     'created': "Membre depuis", "is_admin": "Est admin"}
-    # exlure le mot de passe crypté
-    form_excluded_columns = ('password_hash')
-    # Ajout d'un input password et confirm pour crypter à la validation
-    form_extra_fields = {
+    form_excluded_columns = ('password_hash', 'posts')
+    form_extra_fields = {  # Ajout d'un input password et confirm pour crypter à la validation
         'password': PasswordField(
             'Nouveau mot de passe',
             validators=[EqualTo('confirm', message="Les mots de passe doivent correspondre")]),
         'confirm': PasswordField('Confirmer le nouveau mot de passe')
     }
 
-    column_formatters = {
-        'created': _format_created
-    }
+    can_view_details = True
 
     def create_form(self, obj=None):
         return CreateUserForm()
@@ -74,15 +79,23 @@ class UserAdmin(AdminModelView):
                 return False
         return super(UserAdmin, self).delete_model(model)
 
+    @expose('/<int:id>')
+    def details_view(self, id):
+        user = User.query.get_or_404(id)
+        return_url = self.get_url('.index_view')
+        return self.render('admin/details_user.html', user=user, return_url=return_url)
+
 
 class PostAdmin(AdminModelView):
-    column_list = ('user_id', 'title', 'content')
-    column_labels = {'user_id': 'ID Auteurice',
-                     'title': 'Titre', 'content': 'Contenu'}
-    form_extra_fields = {
-        'user': QuerySelectField('Auteurice', query_factory=lambda: User.query, get_label='username')
+    column_formatters = {
+        'created': lambda v, c, m, p: m.created.strftime("%d-%m-%Y %H:%M"),
+        'user': lambda v, c, m, p: User.query.get(m.user_id).username
     }
+    column_labels = {'user': 'Auteurice',
+                     'title': 'Titre', 'created': "Mise en ligne", "content": "Contenu"}
 
-    def on_model_change(self, form, model, is_created):
-        model.user_id = form.user.data.id
-        return super(PostAdmin, self).on_model_change(form, model, is_created)
+    column_list = ('user', 'title', 'created')
+    
+    can_view_details = True
+    column_details_list = ('user', 'created', 'title', 'content')
+
