6ae4b0d61a73b435065cf037583473a245d74e22
gaellerem <146330927+gaellerem@users.noreply.github.com>
2024-09-17 20:15:09 +0200

    added author to post view

--- data/populate_db.py
+++ data/populate_db.py
@@ -13,7 +13,7 @@ def random_date(start, end):
 def populate_users_db_from_json(json_file):
     with open(json_file, 'r', encoding='utf-8') as file:
         data = json.load(file)
-    
+
     start_date = datetime(datetime.now().year, 1, 1)
     end_date = datetime.now()
 
